a318622dc9f761bd3cbccd28fb064f780c158c3d
Murat Caliskan <muratcnjid@hotmail.com>
2018-03-28 20:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  var lis = document.querySelectorAll('ul.ranked-list')
+  var lis = document.querySelectorAll('ul.ranked-list li')
  
 for (let i = 0; i < lis.length; i++) {
   return parseInt(lis[i])+n;
